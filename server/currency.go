2cc787a7dc48e3e71acad104ae27dc536f7a741f
dolmatovDan <58562604+dolmatovDan@users.noreply.github.com>
2025-04-06 10:38:50 +0300

    add bi-directional streaming

--- server/currency.go
+++ server/currency.go
@@ -2,16 +2,19 @@ package server
 
 import (
 	"context"
+	"io"
+	"time"
 
 	"github.com/dolmatovDan/gRPC/currency"
 	"github.com/dolmatovDan/gRPC/data"
+	"google.golang.org/grpc"
 
 	"github.com/hashicorp/go-hclog"
 )
 
 type Currency struct {
 	rates *data.ExchangeRates
-	log hclog.Logger
+	log   hclog.Logger
 	currency.UnimplementedCurrencyServer
 }
 
@@ -26,10 +29,34 @@ func (c *Currency) GetRate(ctx context.Context, rr *currency.RateRequest) (*curr
 	return &currency.RateResponse{Rate: rate}, nil
 }
 
-func NewCurrency(r *data.ExchangeRates,l hclog.Logger) *Currency {
+func NewCurrency(r *data.ExchangeRates, l hclog.Logger) *Currency {
 	return &Currency{r, l, currency.UnimplementedCurrencyServer{}}
 }
 
-func (c*Currency) SubscribeRates(protos Currency_SubscribeRatesServer) {
+func (c *Currency) SubscribeRates(src grpc.BidiStreamingServer[currency.RateRequest, currency.RateResponse]) error {
 
+	go func() {
+		for {
+			rr, err := src.Recv()
+			if err == io.EOF {
+				c.log.Info("Clieant has closed connection")
+				break
+			}
+			if err != nil {
+				c.log.Error("Unable to read from client", "error", err)
+				break
+			}
+
+			c.log.Info("Handle client request", "request", rr)
+		}
+	}()
+
+	for {
+		err := src.Send(&currency.RateResponse{Rate: 12.1})
+		if err != nil {
+			return err
+		}
+
+		time.Sleep(5 * time.Second)
+	}
 }
